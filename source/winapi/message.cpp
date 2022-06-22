e32ebe3904683ae3350ad283f7621dde8c787daf
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2022-06-22 08:47:14 -0300

    Update message.cpp

--- source/winapi/message.cpp
+++ source/winapi/message.cpp
@@ -12,84 +12,77 @@
 #include <commctrl.h>
 #include <richedit.h>
 
-static int s_msgbox( UINT uType )
+static int s_msgbox(UINT uType)
 {
-   void *hText, *hTitle;
-   int iResult;
-
-   iResult = MessageBox( GetActiveWindow(),
-         HB_PARSTR(1, &hText, nullptr),
-         HB_PARSTRDEF( 2, &hTitle, nullptr ), uType );
+   void * hText;
+   void * hTitle;
+   int iResult = MessageBox(GetActiveWindow(), HB_PARSTR(1, &hText, nullptr), HB_PARSTRDEF(2, &hTitle, nullptr), uType);
    hb_strfree(hText);
    hb_strfree(hTitle);
-
    return iResult;
 }
 
 HB_FUNC( HWG_MSGINFO )
 {
-   s_msgbox( MB_OK | MB_ICONINFORMATION );
+   s_msgbox(MB_OK | MB_ICONINFORMATION);
 }
 
 HB_FUNC( HWG_MSGSTOP )
 {
-   s_msgbox( MB_OK | MB_ICONSTOP );
+   s_msgbox(MB_OK | MB_ICONSTOP);
 }
 
 HB_FUNC( HWG_MSGOKCANCEL )
 {
-   hb_retni( s_msgbox( MB_OKCANCEL | MB_ICONQUESTION ) );
+   hb_retni(s_msgbox(MB_OKCANCEL | MB_ICONQUESTION));
 }
 
 HB_FUNC( HWG_MSGYESNO )
 {
-   hb_retl( s_msgbox( MB_YESNO | MB_ICONQUESTION ) == IDYES );
+   hb_retl(s_msgbox(MB_YESNO | MB_ICONQUESTION) == IDYES);
 }
 
 HB_FUNC( HWG_MSGNOYES )
 {
-   hb_retl( s_msgbox( MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2 ) == IDYES );
+   hb_retl(s_msgbox(MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2) == IDYES);
 }
 
 HB_FUNC( HWG_MSGYESNOCANCEL )
 {
-   int iResult = s_msgbox( MB_YESNOCANCEL | MB_ICONQUESTION );
-   hb_retni( (iResult==6)? 1 : ( (iResult==7)? 2 : 0 ) );
+   int iResult = s_msgbox(MB_YESNOCANCEL | MB_ICONQUESTION);
+   hb_retni((iResult == 6) ? 1 : ((iResult == 7) ? 2 : 0));
 }
 
 HB_FUNC( HWG_MSGEXCLAMATION )
 {
-   s_msgbox( MB_ICONEXCLAMATION | MB_OK | MB_SYSTEMMODAL );
+   s_msgbox(MB_ICONEXCLAMATION | MB_OK | MB_SYSTEMMODAL);
 }
 
 HB_FUNC( HWG_MSGRETRYCANCEL )
 {
-   hb_retni( s_msgbox( MB_RETRYCANCEL | MB_ICONQUESTION | MB_ICONQUESTION ) );
+   hb_retni(s_msgbox(MB_RETRYCANCEL | MB_ICONQUESTION | MB_ICONQUESTION));
 }
 
 HB_FUNC( HWG_MSGBEEP )
 {
-   MessageBeep( ( hb_pcount() ==
-               0 ) ? ( LONG ) 0xFFFFFFFF : hb_parnl(1) );
+   MessageBeep((hb_pcount() == 0) ? static_cast<LONG>(0xFFFFFFFF) : hb_parnl(1));
 }
 
 /*
  Function hwg_MsgTemp()
  (Not for HWGUI applications, only for debugging)
- 
+
  This function displays some compiler constants
  in a Messagebox "DialogBaseUnits".
- 
+
  Table: Constants and header file with declaration (MinGW), default value and remarks:
 
   CP_ACP                   winnls.h     0
   WS_OVERLAPPEDWINDOW      winuser.h    0xcf0000
   NM_FIRST                 commctrl.h   0
-  
+
   The function MultiByteToWideChar(UINT,DWORD,LPCSTR,int,LPWSTR,int)
   converts LPCSTR to LPWSTR strings.
-  
-  
 */
 
 HB_FUNC( HWG_MSGTEMP )
@@ -98,27 +91,18 @@ HB_FUNC( HWG_MSGTEMP )
    LPCTSTR msg;
 
 #if __HARBOUR__ - 0 >= 0x010100
-   hb_snprintf( cres, sizeof(cres),
-         "WS_OVERLAPPEDWINDOW: %lx NM_FIRST: %d ",
-         ( LONG ) WS_OVERLAPPEDWINDOW, NM_FIRST );
+   hb_snprintf(cres, sizeof(cres), "WS_OVERLAPPEDWINDOW: %lx NM_FIRST: %d ", static_cast<LONG>(WS_OVERLAPPEDWINDOW), NM_FIRST);
 #else
-   sprintf( cres, "WS_OVERLAPPEDWINDOW: %lx NM_FIRST: %d ",
-         ( LONG ) WS_OVERLAPPEDWINDOW, NM_FIRST );
+   sprintf(cres, "WS_OVERLAPPEDWINDOW: %lx NM_FIRST: %d ", static_cast<LONG>(WS_OVERLAPPEDWINDOW), NM_FIRST);
 #endif
    {
 #ifdef UNICODE
       TCHAR wcres[60];
-      MultiByteToWideChar( CP_ACP, 0, cres, -1, wcres,
-            HB_SIZEOFARRAY( wcres ) );
+      MultiByteToWideChar(CP_ACP, 0, cres, -1, wcres, HB_SIZEOFARRAY(wcres));
       msg = wcres;
 #else
       msg = cres;
 #endif
-      hb_retni( MessageBox( GetActiveWindow(), msg,
-                  TEXT( "DialogBaseUnits" ),
-                  MB_OKCANCEL | MB_ICONQUESTION ) );
+      hb_retni(MessageBox(GetActiveWindow(), msg, TEXT("DialogBaseUnits"), MB_OKCANCEL | MB_ICONQUESTION));
    }
 }
-
-/* ===================== EOF of message.c ==================== */
-
