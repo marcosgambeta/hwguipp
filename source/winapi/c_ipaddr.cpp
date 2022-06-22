f0effbf6d70cead7de09b64c810994ce28c15197
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2022-06-22 08:46:36 -0300

    Update c_ipaddr.cpp

--- source/winapi/c_ipaddr.cpp
+++ source/winapi/c_ipaddr.cpp
@@ -8,18 +8,18 @@
  Copyright 2002 Roberto Lopez <roblez@ciudad.com.ar>
  http://www.geocities.com/harbour_minigui/
 
- This program is free software; you can redistribute it and/or modify it under 
- the terms of the GNU General Public License as published by the Free Software 
- Foundation; either version 2 of the License, or (at your option) any later 
- version. 
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later
+ version.
 
- This program is distributed in the hope that it will be useful, but WITHOUT 
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License along with
- this software; see the file COPYING. If not, write to the Free Software 
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA (or 
+ this software; see the file COPYING. If not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA (or
  visit the web site http://www.gnu.org/).
 
  As a special exception, you have permission for additional uses of the text
@@ -28,7 +28,7 @@
  The exception is that, if you link the Harbour Minigui library with other
  files to produce an executable, this does not by itself cause the resulting
  executable to be covered by the GNU General Public License.
- Your use of that executable is in no way restricted on account of linking the 
+ Your use of that executable is in no way restricted on account of linking the
  Harbour-Minigui library code into it.
 
  Parts of this project are based upon:
@@ -62,53 +62,43 @@ HB_FUNC_EXTERN( HWG_INITCOMMONCONTROLSEX );
 
 HB_FUNC( HWG_INITIPADDRESS )
 {
-   HWND hIpAddress;
-
    HB_FUNC_EXEC( HWG_INITCOMMONCONTROLSEX );
 
-   hIpAddress = CreateWindowEx( WS_EX_CLIENTEDGE, WC_IPADDRESS, TEXT( "" ),
-         hb_parni(3),
-         hb_parni(4), hb_parni(5), hb_parni(6), hb_parni(7),
-         static_cast<HWND>(HB_PARHANDLE(1)), reinterpret_cast<HMENU>(static_cast<UINT_PTR>(hb_parni(2))),
-         GetModuleHandle( nullptr ), nullptr );
+   HWND hIpAddress = CreateWindowEx(WS_EX_CLIENTEDGE, WC_IPADDRESS, TEXT(""), hb_parni(3), hb_parni(4), hb_parni(5), hb_parni(6), hb_parni(7),
+      static_cast<HWND>(HB_PARHANDLE(1)), reinterpret_cast<HMENU>(static_cast<UINT_PTR>(hb_parni(2))), GetModuleHandle(nullptr), nullptr);
 
-   HB_RETHANDLE( hIpAddress );
+   HB_RETHANDLE(hIpAddress);
 }
 
 HB_FUNC( HWG_SETIPADDRESS )
 {
-   BYTE v1, v2, v3, v4;
+   BYTE v1 = static_cast<BYTE>(hb_parni(2));
+   BYTE v2 = static_cast<BYTE>(hb_parni(3));
+   BYTE v3 = static_cast<BYTE>(hb_parni(4));
+   BYTE v4 = static_cast<BYTE>(hb_parni(5));
 
-   v1 = static_cast<BYTE>(hb_parni(2));
-   v2 = static_cast<BYTE>(hb_parni(3));
-   v3 = static_cast<BYTE>(hb_parni(4));
-   v4 = static_cast<BYTE>(hb_parni(5));
-
-   SendMessage( static_cast<HWND>(HB_PARHANDLE(1)), IPM_SETADDRESS, 0,
-         MAKEIPADDRESS( v1, v2, v3, v4 ) );
+   SendMessage(static_cast<HWND>(HB_PARHANDLE(1)), IPM_SETADDRESS, 0, MAKEIPADDRESS(v1, v2, v3, v4));
 }
 
 HB_FUNC( HWG_GETIPADDRESS )
 {
    DWORD pdwAddr;
-   BYTE v1, v2, v3, v4;
 
-   SendMessage( static_cast<HWND>(HB_PARHANDLE(1)), IPM_GETADDRESS, 0,
-         ( LPARAM ) ( LPDWORD ) & pdwAddr );
+   SendMessage(static_cast<HWND>(HB_PARHANDLE(1)), IPM_GETADDRESS, 0, reinterpret_cast<LPARAM>(static_cast<LPDWORD>(&pdwAddr)));
 
-   v1 = static_cast<BYTE>(FIRST_IPADDRESS( pdwAddr ));
-   v2 = static_cast<BYTE>(SECOND_IPADDRESS( pdwAddr ));
-   v3 = static_cast<BYTE>(THIRD_IPADDRESS( pdwAddr ));
-   v4 = static_cast<BYTE>(FOURTH_IPADDRESS( pdwAddr ));
+   BYTE v1 = static_cast<BYTE>(FIRST_IPADDRESS(pdwAddr));
+   BYTE v2 = static_cast<BYTE>(SECOND_IPADDRESS(pdwAddr));
+   BYTE v3 = static_cast<BYTE>(THIRD_IPADDRESS(pdwAddr));
+   BYTE v4 = static_cast<BYTE>(FOURTH_IPADDRESS(pdwAddr));
 
    hb_reta(4);
-   hb_storvni( ( INT ) v1, -1, 1 );
-   hb_storvni( ( INT ) v2, -1, 2 );
-   hb_storvni( ( INT ) v3, -1, 3 );
-   hb_storvni( ( INT ) v4, -1, 4 );
+   hb_storvni(static_cast<INT>(v1), -1, 1);
+   hb_storvni(static_cast<INT>(v2), -1, 2);
+   hb_storvni(static_cast<INT>(v3), -1, 3);
+   hb_storvni(static_cast<INT>(v4), -1, 4);
 }
 
 HB_FUNC( HWG_CLEARIPADDRESS )
 {
-   SendMessage( static_cast<HWND>(HB_PARHANDLE(1)), IPM_CLEARADDRESS, 0, 0 );
+   SendMessage(static_cast<HWND>(HB_PARHANDLE(1)), IPM_CLEARADDRESS, 0, 0);
 }
