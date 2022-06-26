ca2b00ce76255100cf20ab167a3b0af4b0f387b7
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2022-06-26 17:14:04 -0300

    Add edit.cpp

--- source/winapi/control.cpp
+++ source/winapi/control.cpp
@@ -51,7 +51,6 @@ HB_EXTERN_END
 LRESULT CALLBACK WinCtrlProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT APIENTRY SplitterProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT APIENTRY StaticSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-LRESULT APIENTRY EditSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT APIENTRY ListSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT APIENTRY TrackSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT APIENTRY TreeViewSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -60,7 +59,7 @@ static void CALLBACK s_timerProc(HWND, UINT, UINT, DWORD);
 static HWND s_hWndTT = nullptr;
 static bool s_lInitCmnCtrl = false;
 static bool s_lToolTipBalloon = false;
-static WNDPROC wpOrigEditProc, wpOrigTrackProc, wpOrigStaticProc, wpOrigListProc, wpOrigTreeViewProc;
+static WNDPROC wpOrigTrackProc, wpOrigStaticProc, wpOrigListProc, wpOrigTreeViewProc;
 
 /*
 HWG_INITCOMMONCONTROLSEX() --> NIL
@@ -222,36 +221,6 @@ HB_FUNC( HWG_CREATESTATIC )
     */
 
    HB_RETHANDLE(hWndCtrl);
-}
-
-/*
-   CreateEdit(hParentWIndow, nEditControlID, nStyle, x, y, nWidth, nHeight, cInitialString)
-*/
-HB_FUNC( HWG_CREATEEDIT )
-{
-   ULONG ulStyle = hb_parnl(3);
-   ULONG ulStyleEx = (ulStyle & WS_BORDER) ? WS_EX_CLIENTEDGE : 0;
-   HWND hWndEdit;
-
-   if( (ulStyle & WS_BORDER) ) // && (ulStyle & WS_DLGFRAME) )
-   {
-      ulStyle &= ~WS_BORDER;
-   }
-   hWndEdit = CreateWindowEx(ulStyleEx, TEXT("EDIT"), nullptr, WS_CHILD | WS_VISIBLE | ulStyle, hb_parni(4), hb_parni(5), hb_parni(6), hb_parni(7),
-      static_cast<HWND>(HB_PARHANDLE(1)), reinterpret_cast<HMENU>(static_cast<UINT_PTR>(hb_parni(2))), GetModuleHandle(nullptr), nullptr);
-
-   if( hb_pcount() > 7 )
-   {
-      void * hStr;
-      LPCTSTR lpText = HB_PARSTR(8, &hStr, nullptr);
-      if( lpText )
-      {
-         SendMessage(hWndEdit, WM_SETTEXT, 0, reinterpret_cast<LPARAM>(lpText));
-      }
-      hb_strfree(hStr);
-   }
-
-   HB_RETHANDLE(hWndEdit);
 }
 
 /*
@@ -1128,55 +1097,6 @@ LRESULT APIENTRY StaticSubclassProc(HWND hWnd, UINT message, WPARAM wParam, LPAR
    else
    {
       return (CallWindowProc(wpOrigStaticProc, hWnd, message, wParam, lParam));
-   }
-}
-
-
-HB_FUNC( HWG_INITEDITPROC )
-{
-   wpOrigEditProc = reinterpret_cast<WNDPROC>(SetWindowLongPtr(static_cast<HWND>(HB_PARHANDLE(1)), GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(EditSubclassProc)));
-}
-
-LRESULT APIENTRY EditSubclassProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-   long int res;
-   PHB_ITEM pObject = reinterpret_cast<PHB_ITEM>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
-
-   if( !pSym_onEvent )
-   {
-      pSym_onEvent = hb_dynsymFindName("ONEVENT");
-   }
-
-   if( pSym_onEvent && pObject )
-   {
-      hb_vmPushSymbol(hb_dynsymSymbol(pSym_onEvent));
-      hb_vmPush(pObject);
-      hb_vmPushLong(static_cast<LONG>(message));
-//      hb_vmPushLong(static_cast<LONG>(wParam));
-//      hb_vmPushLong(static_cast<LONG>(lParam));
-      HB_PUSHITEM(wParam);
-      HB_PUSHITEM(lParam);
-      hb_vmSend(3);
-      if( HB_ISPOINTER(-1) )
-      {
-         return reinterpret_cast<LRESULT>(HB_PARHANDLE(-1));
-      }
-      else
-      {
-         res = hb_parnl(-1);
-         if( res == -1 )
-         {
-            return (CallWindowProc(wpOrigEditProc, hWnd, message, wParam, lParam));
-         }
-         else
-         {
-            return res;
-         }
-      }
-   }
-   else
-   {
-      return (CallWindowProc(wpOrigEditProc, hWnd, message, wParam, lParam));
    }
 }
 
---
+++ source/winapi/edit.cpp
@@ -0,0 +1,117 @@
+/*
+ * HWGUI - Harbour Win32 GUI library source code:
+ * C level controls functions
+ *
+ * Copyright 2001 Alexander S.Kresin <alex@kresin.ru>
+ * www - http://www.kresin.ru
+*/
+
+#define HB_OS_WIN_32_USED
+
+#define OEMRESOURCE
+#include "hwingui.h"
+#include <commctrl.h>
+#include <winuser.h>
+
+#include "hbapiitm.h"
+#include "hbvm.h"
+#include "hbdate.h"
+#include "hbtrace.h"
+
+/* Suppress compiler warnings */
+#include "incomp_pointer.h"
+#include "warnings.h"
+
+LRESULT APIENTRY EditSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+static WNDPROC wpOrigEditProc;
+
+/*
+HWG_CREATEEDIT(hParentWIndow, nEditControlID, nStyle, nX, nY, nWidth, nHeight, cInitialString) --> hEdit
+*/
+HB_FUNC( HWG_CREATEEDIT )
+{
+   ULONG ulStyle = hb_parnl(3);
+   ULONG ulStyleEx = (ulStyle & WS_BORDER) ? WS_EX_CLIENTEDGE : 0;
+   HWND hWndEdit;
+
+   if( (ulStyle & WS_BORDER) ) // && (ulStyle & WS_DLGFRAME) )
+   {
+      ulStyle &= ~WS_BORDER;
+   }
+
+   hWndEdit = CreateWindowEx(ulStyleEx,
+                             TEXT("EDIT"),
+                             nullptr,
+                             WS_CHILD | WS_VISIBLE | ulStyle,
+                             hb_parni(4),
+                             hb_parni(5),
+                             hb_parni(6),
+                             hb_parni(7),
+                             static_cast<HWND>(HB_PARHANDLE(1)),
+                             reinterpret_cast<HMENU>(static_cast<UINT_PTR>(hb_parni(2))),
+                             GetModuleHandle(nullptr),
+                             nullptr
+                             );
+
+   if( hb_pcount() > 7 )
+   {
+      void * hStr;
+      LPCTSTR lpText = HB_PARSTR(8, &hStr, nullptr);
+      if( lpText )
+      {
+         SendMessage(hWndEdit, WM_SETTEXT, 0, reinterpret_cast<LPARAM>(lpText));
+      }
+      hb_strfree(hStr);
+   }
+
+   HB_RETHANDLE(hWndEdit);
+}
+
+HB_FUNC( HWG_INITEDITPROC )
+{
+   wpOrigEditProc = reinterpret_cast<WNDPROC>(SetWindowLongPtr(static_cast<HWND>(HB_PARHANDLE(1)), GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(EditSubclassProc)));
+}
+
+LRESULT APIENTRY EditSubclassProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+   long int res;
+   PHB_ITEM pObject = reinterpret_cast<PHB_ITEM>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+
+   if( !pSym_onEvent )
+   {
+      pSym_onEvent = hb_dynsymFindName("ONEVENT");
+   }
+
+   if( pSym_onEvent && pObject )
+   {
+      hb_vmPushSymbol(hb_dynsymSymbol(pSym_onEvent));
+      hb_vmPush(pObject);
+      hb_vmPushLong(static_cast<LONG>(message));
+//      hb_vmPushLong(static_cast<LONG>(wParam));
+//      hb_vmPushLong(static_cast<LONG>(lParam));
+      HB_PUSHITEM(wParam);
+      HB_PUSHITEM(lParam);
+      hb_vmSend(3);
+      if( HB_ISPOINTER(-1) )
+      {
+         return reinterpret_cast<LRESULT>(HB_PARHANDLE(-1));
+      }
+      else
+      {
+         res = hb_parnl(-1);
+         if( res == -1 )
+         {
+            return (CallWindowProc(wpOrigEditProc, hWnd, message, wParam, lParam));
+         }
+         else
+         {
+            return res;
+         }
+      }
+   }
+   else
+   {
+      return (CallWindowProc(wpOrigEditProc, hWnd, message, wParam, lParam));
+   }
+}
