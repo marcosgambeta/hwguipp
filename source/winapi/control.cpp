6cae16a8e4676e95c0fc3e118e13c260b9dd295d
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2022-06-26 08:55:25 -0300

    Add tab.cpp

--- source/winapi/control.cpp
+++ source/winapi/control.cpp
@@ -57,14 +57,13 @@ LRESULT APIENTRY ListSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lP
 LRESULT APIENTRY UpDownSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT APIENTRY DatePickerSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT APIENTRY TrackSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-LRESULT APIENTRY TabSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT APIENTRY TreeViewSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 static void CALLBACK s_timerProc(HWND, UINT, UINT, DWORD);
 
 static HWND s_hWndTT = nullptr;
 static bool s_lInitCmnCtrl = false;
 static bool s_lToolTipBalloon = false;
-static WNDPROC wpOrigEditProc, wpOrigTrackProc, wpOrigTabProc, wpOrigStaticProc, wpOrigListProc, wpOrigUpDownProc, wpOrigDatePickerProc,  wpOrigTreeViewProc;     //wpOrigButtonProc
+static WNDPROC wpOrigEditProc, wpOrigTrackProc, wpOrigStaticProc, wpOrigListProc, wpOrigUpDownProc, wpOrigDatePickerProc,  wpOrigTreeViewProc;     //wpOrigButtonProc
 static LONG_PTR wpOrigButtonProc;
 
 /*
@@ -601,126 +600,6 @@ HB_FUNC( HWG_GETTIMEPICKER )
    //sprintf(szTime, "%02d:%02d:%02d", st.wHour, st.wMinute, st.wSecond);
    hb_snprintf(szTime, 9, "%02d:%02d:%02d", st.wHour, st.wMinute, st.wSecond);
    hb_retc(szTime);
-}
-
-/*
-HWG_CREATETABCONTROL(hParent, nPar2, nStyle, nX, nY, nWidth, nHeight) --> hTab
-*/
-HB_FUNC( HWG_CREATETABCONTROL )
-{
-   HWND hTab = CreateWindow(WC_TABCONTROL, nullptr, WS_CHILD | WS_VISIBLE | hb_parnl(3),
-         hb_parni(4), hb_parni(5), hb_parni(6), hb_parni(7), static_cast<HWND>(HB_PARHANDLE(1)),
-         reinterpret_cast<HMENU>(static_cast<UINT_PTR>(hb_parni(2))),
-         GetModuleHandle(nullptr), nullptr);
-   HB_RETHANDLE(hTab);
-}
-
-HB_FUNC( HWG_INITTABCONTROL )
-{
-   HWND hTab = static_cast<HWND>(HB_PARHANDLE(1));
-   PHB_ITEM pArr = hb_param(2, HB_IT_ARRAY);
-   int iItems = hb_parnl(3);
-   TC_ITEM tie;
-   ULONG ulTabs = hb_arrayLen(pArr);
-
-   tie.mask = TCIF_TEXT | TCIF_IMAGE;
-   tie.iImage = iItems == 0 ? -1 : 0;
-
-   for( ULONG ul = 1; ul <= ulTabs; ul++ )
-   {
-      void * hStr;
-
-      tie.pszText = const_cast<LPTSTR>(HB_ARRAYGETSTR(pArr, ul, &hStr, nullptr));
-      if( tie.pszText == nullptr )
-      {
-         tie.pszText = const_cast<LPTSTR>(TEXT(""));
-      }
-
-      if( TabCtrl_InsertItem(hTab, ul - 1, &tie) == -1 )
-      {
-         DestroyWindow(hTab);
-         hTab = nullptr;
-      }
-      hb_strfree(hStr);
-
-      if( tie.iImage > -1 )
-      {
-         tie.iImage++;
-      }
-   }
-}
-
-HB_FUNC( HWG_ADDTAB )
-{
-   TC_ITEM tie;
-   void * hStr;
-   tie.mask = TCIF_TEXT | TCIF_IMAGE;
-   tie.iImage = -1;
-   tie.pszText = const_cast<LPTSTR>(HB_PARSTR(3, &hStr, nullptr));
-   TabCtrl_InsertItem(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2), &tie);
-   hb_strfree(hStr);
-}
-
-HB_FUNC( HWG_ADDTABDIALOG )
-{
-   TC_ITEM tie;
-   void * hStr;
-   HWND pWnd = static_cast<HWND>(HB_PARHANDLE(4));
-
-   tie.mask = TCIF_TEXT | TCIF_IMAGE | TCIF_PARAM;
-   tie.lParam = reinterpret_cast<LPARAM>(pWnd);
-   tie.iImage = -1;
-   tie.pszText = const_cast<LPTSTR>(HB_PARSTR(3, &hStr, nullptr));
-   TabCtrl_InsertItem(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2), &tie);
-   hb_strfree(hStr);
-}
-
-HB_FUNC( HWG_DELETETAB )
-{
-   TabCtrl_DeleteItem(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2));
-}
-
-HB_FUNC( HWG_GETCURRENTTAB )
-{
-   hb_retni(TabCtrl_GetCurSel( static_cast<HWND>(HB_PARHANDLE(1)) ) + 1);
-}
-
-HB_FUNC( HWG_SETTABSIZE )
-{
-   SendMessage(static_cast<HWND>(HB_PARHANDLE(1)), TCM_SETITEMSIZE, 0, MAKELPARAM(hb_parni(2), hb_parni(3)));
-}
-
-HB_FUNC( HWG_SETTABNAME )
-{
-   TC_ITEM tie;
-   void * hStr;
-   tie.mask = TCIF_TEXT;
-   tie.pszText = const_cast<LPTSTR>(HB_PARSTR(3, &hStr, nullptr));
-   TabCtrl_SetItem(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2)-1, &tie);
-   hb_strfree(hStr);
-}
-
-HB_FUNC( HWG_TAB_HITTEST )
-{
-   TC_HITTESTINFO ht;
-   HWND hTab = static_cast<HWND>(HB_PARHANDLE(1));
-   int res;
-
-   if( hb_pcount() > 1 && HB_ISNUM(2) && HB_ISNUM(3) )
-   {
-      ht.pt.x = hb_parni(2);
-      ht.pt.y = hb_parni(3);
-   }
-   else
-   {
-      GetCursorPos(&(ht.pt));
-      ScreenToClient(hTab, &(ht.pt));
-   }
-
-   res = static_cast<int>(SendMessage(hTab, TCM_HITTEST, 0, reinterpret_cast<LPARAM>(&ht)));
-
-   hb_storni(ht.flags, 4);
-   hb_retni(res);
 }
 
 HB_FUNC( HWG_GETNOTIFYKEYDOWN )
@@ -1700,54 +1579,6 @@ LRESULT APIENTRY TrackSubclassProc(HWND hWnd, UINT message, WPARAM wParam, LPARA
    }
 }
 
-HB_FUNC( HWG_INITTABPROC )
-{
-   wpOrigTabProc = reinterpret_cast<WNDPROC>(SetWindowLongPtr(static_cast<HWND>(HB_PARHANDLE(1)), GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(TabSubclassProc)));
-}
-
-LRESULT APIENTRY TabSubclassProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-   long int res;
-   PHB_ITEM pObject = reinterpret_cast<PHB_ITEM>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
-
-   if( !pSym_onEvent )
-   {
-      pSym_onEvent = hb_dynsymFindName("ONEVENT");
-   }
-
-   if( pSym_onEvent && pObject )
-   {
-      hb_vmPushSymbol(hb_dynsymSymbol(pSym_onEvent));
-      hb_vmPush(pObject);
-      hb_vmPushLong(static_cast<LONG>(message));
-//      hb_vmPushLong(static_cast<LONG>(wParam));
-//      hb_vmPushLong(static_cast<LONG>(lParam));
-      HB_PUSHITEM(wParam);
-      HB_PUSHITEM(lParam);
-      hb_vmSend(3);
-      if( HB_ISPOINTER(-1) )
-      {
-         return reinterpret_cast<LRESULT>(HB_PARHANDLE(-1));
-      }
-      else
-      {
-         res = hb_parnl(-1);
-         if( res == -1 )
-         {
-            return (CallWindowProc(wpOrigTabProc, hWnd, message, wParam, lParam));
-         }
-         else
-         {
-            return res;
-         }
-      }
-   }
-   else
-   {
-      return (CallWindowProc(wpOrigTabProc, hWnd, message, wParam, lParam));
-   }
-}
-
 HB_FUNC( HWG_CREATEPAGER )
 {
    BOOL bVert = hb_parl(8);
@@ -1938,25 +1769,6 @@ HB_FUNC( HWG_GETDLGMESSAGE )
       }
    }
    hb_retnl(0);
-}
-
-HB_FUNC( HWG_TABITEMPOS )
-{
-   RECT pRect;
-   TabCtrl_GetItemRect(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2), &pRect);
-   hb_itemRelease(hb_itemReturn(Rect2Array(&pRect)));
-}
-
-HB_FUNC( HWG_GETTABNAME )
-{
-   TC_ITEM tie;
-   TCHAR d[255] = { 0 };
-
-   tie.mask = TCIF_TEXT;
-   tie.cchTextMax = HB_SIZEOFARRAY(d) - 1;
-   tie.pszText = d;
-   TabCtrl_GetItem(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2) - 1, static_cast<LPTCITEM>(&tie));
-   HB_RETSTR(tie.pszText);
 }
 
 HB_FUNC( HWG_GETUTCTIMEDATE ) /* Format: W,YYYYMMDD-HH:MM:SS */
---
+++ source/winapi/tab.cpp
@@ -0,0 +1,222 @@
+/*
+ * HWGUI - Harbour Win32 GUI library source code:
+ * C level controls functions
+ *
+ * Copyright 2001 Alexander S.Kresin <alex@kresin.ru>
+ * www - http://www.kresin.ru
+*/
+
+#define HB_OS_WIN_32_USED
+
+#define OEMRESOURCE
+#include "hwingui.h"
+#include <commctrl.h>
+#include <winuser.h>
+
+#include "hbapiitm.h"
+#include "hbvm.h"
+#include "hbdate.h"
+#include "hbtrace.h"
+
+/* Suppress compiler warnings */
+#include "incomp_pointer.h"
+#include "warnings.h"
+
+LRESULT APIENTRY TabSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+static WNDPROC wpOrigTabProc;
+
+/*
+HWG_CREATETABCONTROL(hParent, nID, nStyle, nX, nY, nWidth, nHeight) --> hTab
+*/
+HB_FUNC( HWG_CREATETABCONTROL )
+{
+   HWND hTab = CreateWindow(WC_TABCONTROL,
+                            nullptr,
+                            WS_CHILD | WS_VISIBLE | hb_parnl(3),
+                            hb_parni(4),
+                            hb_parni(5),
+                            hb_parni(6),
+                            hb_parni(7),
+                            static_cast<HWND>(HB_PARHANDLE(1)),
+                            reinterpret_cast<HMENU>(static_cast<UINT_PTR>(hb_parni(2))),
+                            GetModuleHandle(nullptr),
+                            nullptr
+                            );
+   HB_RETHANDLE(hTab);
+}
+
+HB_FUNC( HWG_INITTABCONTROL )
+{
+   HWND hTab = static_cast<HWND>(HB_PARHANDLE(1));
+   PHB_ITEM pArr = hb_param(2, HB_IT_ARRAY);
+   int iItems = hb_parnl(3);
+   TC_ITEM tie;
+   ULONG ulTabs = hb_arrayLen(pArr);
+
+   tie.mask = TCIF_TEXT | TCIF_IMAGE;
+   tie.iImage = iItems == 0 ? -1 : 0;
+
+   for( ULONG ul = 1; ul <= ulTabs; ul++ )
+   {
+      void * hStr;
+
+      tie.pszText = const_cast<LPTSTR>(HB_ARRAYGETSTR(pArr, ul, &hStr, nullptr));
+      if( tie.pszText == nullptr )
+      {
+         tie.pszText = const_cast<LPTSTR>(TEXT(""));
+      }
+
+      if( TabCtrl_InsertItem(hTab, ul - 1, &tie) == -1 )
+      {
+         DestroyWindow(hTab);
+         hTab = nullptr;
+      }
+      hb_strfree(hStr);
+
+      if( tie.iImage > -1 )
+      {
+         tie.iImage++;
+      }
+   }
+}
+
+HB_FUNC( HWG_ADDTAB )
+{
+   TC_ITEM tie;
+   void * hStr;
+   tie.mask = TCIF_TEXT | TCIF_IMAGE;
+   tie.iImage = -1;
+   tie.pszText = const_cast<LPTSTR>(HB_PARSTR(3, &hStr, nullptr));
+   TabCtrl_InsertItem(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2), &tie);
+   hb_strfree(hStr);
+}
+
+HB_FUNC( HWG_ADDTABDIALOG )
+{
+   TC_ITEM tie;
+   void * hStr;
+   HWND pWnd = static_cast<HWND>(HB_PARHANDLE(4));
+
+   tie.mask = TCIF_TEXT | TCIF_IMAGE | TCIF_PARAM;
+   tie.lParam = reinterpret_cast<LPARAM>(pWnd);
+   tie.iImage = -1;
+   tie.pszText = const_cast<LPTSTR>(HB_PARSTR(3, &hStr, nullptr));
+   TabCtrl_InsertItem(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2), &tie);
+   hb_strfree(hStr);
+}
+
+HB_FUNC( HWG_DELETETAB )
+{
+   TabCtrl_DeleteItem(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2));
+}
+
+HB_FUNC( HWG_GETCURRENTTAB )
+{
+   hb_retni(TabCtrl_GetCurSel( static_cast<HWND>(HB_PARHANDLE(1)) ) + 1);
+}
+
+HB_FUNC( HWG_SETTABSIZE )
+{
+   SendMessage(static_cast<HWND>(HB_PARHANDLE(1)), TCM_SETITEMSIZE, 0, MAKELPARAM(hb_parni(2), hb_parni(3)));
+}
+
+HB_FUNC( HWG_SETTABNAME )
+{
+   TC_ITEM tie;
+   void * hStr;
+   tie.mask = TCIF_TEXT;
+   tie.pszText = const_cast<LPTSTR>(HB_PARSTR(3, &hStr, nullptr));
+   TabCtrl_SetItem(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2)-1, &tie);
+   hb_strfree(hStr);
+}
+
+HB_FUNC( HWG_TAB_HITTEST )
+{
+   TC_HITTESTINFO ht;
+   HWND hTab = static_cast<HWND>(HB_PARHANDLE(1));
+   int res;
+
+   if( hb_pcount() > 1 && HB_ISNUM(2) && HB_ISNUM(3) )
+   {
+      ht.pt.x = hb_parni(2);
+      ht.pt.y = hb_parni(3);
+   }
+   else
+   {
+      GetCursorPos(&(ht.pt));
+      ScreenToClient(hTab, &(ht.pt));
+   }
+
+   res = static_cast<int>(SendMessage(hTab, TCM_HITTEST, 0, reinterpret_cast<LPARAM>(&ht)));
+
+   hb_storni(ht.flags, 4);
+   hb_retni(res);
+}
+
+HB_FUNC( HWG_TABITEMPOS )
+{
+   RECT pRect;
+   TabCtrl_GetItemRect(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2), &pRect);
+   hb_itemRelease(hb_itemReturn(Rect2Array(&pRect)));
+}
+
+HB_FUNC( HWG_GETTABNAME )
+{
+   TC_ITEM tie;
+   TCHAR d[255] = { 0 };
+
+   tie.mask = TCIF_TEXT;
+   tie.cchTextMax = HB_SIZEOFARRAY(d) - 1;
+   tie.pszText = d;
+   TabCtrl_GetItem(static_cast<HWND>(HB_PARHANDLE(1)), hb_parni(2) - 1, static_cast<LPTCITEM>(&tie));
+   HB_RETSTR(tie.pszText);
+}
+
+HB_FUNC( HWG_INITTABPROC )
+{
+   wpOrigTabProc = reinterpret_cast<WNDPROC>(SetWindowLongPtr(static_cast<HWND>(HB_PARHANDLE(1)), GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(TabSubclassProc)));
+}
+
+LRESULT APIENTRY TabSubclassProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+   long int res;
+   PHB_ITEM pObject = reinterpret_cast<PHB_ITEM>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+
+   if( !pSym_onEvent )
+   {
+      pSym_onEvent = hb_dynsymFindName("ONEVENT");
+   }
+
+   if( pSym_onEvent && pObject )
+   {
+      hb_vmPushSymbol(hb_dynsymSymbol(pSym_onEvent));
+      hb_vmPush(pObject);
+      hb_vmPushLong(static_cast<LONG>(message));
+//      hb_vmPushLong(static_cast<LONG>(wParam));
+//      hb_vmPushLong(static_cast<LONG>(lParam));
+      HB_PUSHITEM(wParam);
+      HB_PUSHITEM(lParam);
+      hb_vmSend(3);
+      if( HB_ISPOINTER(-1) )
+      {
+         return reinterpret_cast<LRESULT>(HB_PARHANDLE(-1));
+      }
+      else
+      {
+         res = hb_parnl(-1);
+         if( res == -1 )
+         {
+            return (CallWindowProc(wpOrigTabProc, hWnd, message, wParam, lParam));
+         }
+         else
+         {
+            return res;
+         }
+      }
+   }
+   else
+   {
+      return (CallWindowProc(wpOrigTabProc, hWnd, message, wParam, lParam));
+   }
+}
