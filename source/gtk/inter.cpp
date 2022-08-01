08bb4c313cff032df7e14f0581e0ca599f229bbd
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2022-08-01 14:56:30 -0300

    Update inter.cpp

--- source/gtk/inter.cpp
+++ source/gtk/inter.cpp
@@ -9,7 +9,6 @@
  * Copyright 2020 Wilfried Brunken, DF7BE
 */
 
-
 #include "guilib.h"
 #include "hbapifs.h"
 #include "hbapiitm.h"
@@ -17,7 +16,6 @@
 #include "item.api"
 #include "hbtypes.h"
 #include "hbwinuni.h"
-
 
 // #include "hwgtk.h"
 #include "hbdate.h"
@@ -52,7 +50,7 @@ WINBASEAPI LANGID WINAPI GetUserDefaultUILanguage(void);
 #endif
 
 /* from hwingui.h */
-extern void hwg_writelog( const char * sFile, const char * sTraceMsg, ... );
+extern void hwg_writelog(const char * sFile, const char * sTraceMsg, ...);
 
 /*
  Some troubleshootings:
@@ -66,22 +64,21 @@ HB_FUNC( HWG_GETLOCALEINFO )
    char * puf = malloc(128);
 #if defined(_WIN32) || defined(_WIN64) || defined(__MINGW32__) || defined(__MINGW64__)
    TCHAR szBuffer[] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"; /* l=20 */
-   GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_SLIST, szBuffer,
-                  HB_SIZEOFARRAY( szBuffer ) );
+   GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SLIST, szBuffer, HB_SIZEOFARRAY(szBuffer));
    strncpy(puf, szBuffer, 24);
-   hb_retc( puf );
-   free ( puf );
+   hb_retc(puf);
+   free(puf);
 #else
    char * lokale;
-   lokale = setlocale(LC_CTYPE,NULL); /* only user setting, LC_ALL displays all locale settings */
-   if( lokale == NULL ) /* Out of memory ? ==> return empty string */
+   lokale = setlocale(LC_CTYPE, nullptr); /* only user setting, LC_ALL displays all locale settings */
+   if( lokale == nullptr ) /* Out of memory ? ==> return empty string */
    {
      lokale = "\0";
    }
    strncpy(puf, lokale, 127);  /* Avoid buffer overflow */
-//   hwg_writelog( NULL ,  puf);
-   hb_retc( puf );
-   free( puf );
+//   hwg_writelog(nullptr, puf);
+   hb_retc(puf);
+   free(puf);
 #endif
 }
 
@@ -91,28 +88,29 @@ HB_FUNC( HWG_GETLOCALEINFON )
 /* returns Windows LCID, type is int.
     Windows only , on other OSs available, returns forever -1. */
    int lio;
-   lio = GetLocaleInfo( LOCALE_USER_DEFAULT, LOCALE_SLIST, NULL, 0 );
+   lio = GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SLIST, nullptr, 0);
    hb_retni(lio);
 #else
-   hb_retni( -1 );
+   hb_retni(-1);
 #endif
 }
 
 HB_FUNC( HWG_GETUTCTIMEDATE )
 {
 /* Format: W,YYYYMMDD-HH:MM:SS */
-  char cst[128] = { 0 };
+  char cst[128] = {0};
   char * puf = malloc(25);
-  int i;
-  for ( i = 0 ; i < 128 ; i++) /* Fill string with zeroes to avoid buffer overflow on LINUX */
+  for( int i = 0 ; i < 128 ; i++ ) /* Fill string with zeroes to avoid buffer overflow on LINUX */
+  {
     cst[i] = '\0';
+  }
 #if defined(_WIN32) || defined(_WIN64) || defined(__MINGW32__) || defined(__MINGW64__)
-  SYSTEMTIME st = { 0 };
+  SYSTEMTIME st = {0};
   GetSystemTime(&st);
-  sprintf(cst,"%01d.%04d%02d%02d-%02d:%02d:%02d",st.wDayOfWeek, st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+  sprintf(cst, "%01d.%04d%02d%02d-%02d:%02d:%02d", st.wDayOfWeek, st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
   strncpy(puf, cst, 24);
-  hb_retc( puf);
-  free( puf);
+  hb_retc(puf);
+  free(puf);
 #else
 /* Note for possible extensions:
     tm.tm_yday;    Days since Jan. 1: 0-365
@@ -120,31 +118,32 @@ HB_FUNC( HWG_GETUTCTIMEDATE )
                      * -1 don't know
    Use localtime ( &T ); for local time
 */
-  time_t T = time( NULL );
-  struct tm tm = * gmtime( &T );
+  time_t T = time(nullptr);
+  struct tm tm = * gmtime(&T);
   /* tm.tm_wday: Days since Sunday (0-6) */
-  sprintf(cst,"%01d,%04d%02d%02d-%02d:%02d:%02d",tm.tm_wday, tm.tm_year + 1900,  tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+  sprintf(cst, "%01d,%04d%02d%02d-%02d:%02d:%02d", tm.tm_wday, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
   strncpy(puf, cst, 24);
-  hb_retc( puf );
-  free( puf );
+  hb_retc(puf);
+  free(puf);
 #endif
 }
 
 HB_FUNC( HWG_GETDATEANSI )
 {
 /* Format: YYYYMMDD, based on local time */
-  char cst[128] = { 0 };
+  char cst[128] = {0};
   char * puf = malloc(25);
-  int i;
-  for ( i = 0 ; i < 128 ; i++) /* Fill string with zeroes to avoid buffer overflow on LINUX */
+  for( int i = 0 ; i < 128 ; i++ ) /* Fill string with zeroes to avoid buffer overflow on LINUX */
+  {
     cst[i] = '\0';
+  }
 #if defined(_WIN32) || defined(_WIN64) || defined(__MINGW32__) || defined(__MINGW64__)
-  SYSTEMTIME lt = { 0 };
+  SYSTEMTIME lt = {0};
   GetLocalTime(&lt);
-  sprintf(cst,"%04d%02d%02d", lt.wYear, lt.wMonth, lt.wDay);
+  sprintf(cst, "%04d%02d%02d", lt.wYear, lt.wMonth, lt.wDay);
   strncpy(puf, cst, 24);
-  hb_retc( puf);
-  free( puf);
+  hb_retc(puf);
+  free(puf);
 #else
 /* Note for possible extensions:
     tm.tm_yday;    Days since Jan. 1: 0-365
@@ -152,47 +151,45 @@ HB_FUNC( HWG_GETDATEANSI )
                      * -1 don't know
    Use localtime ( &T ); for local time
 */
-  time_t T = time( NULL );
-  struct tm tm = * localtime( &T );
+  time_t T = time(nullptr);
+  struct tm tm = * localtime(&T);
   /* tm.tm_wday: Days since Sunday (0-6) */
-  sprintf(cst,"%04d%02d%02d", tm.tm_year + 1900,  tm.tm_mon + 1, tm.tm_mday );
+  sprintf(cst, "%04d%02d%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
   strncpy(puf, cst, 24);
-  hb_retc( puf );
-  free( puf );
+  hb_retc(puf);
+  free(puf);
 #endif
 }
-
 
 HB_FUNC( HWG_DEFUSERLANG )
 /* Windows only , on other OSs available, returns forever "-1". */
 {
   char * puf = malloc(25);
 #if defined(_WIN32) || defined(_WIN64) || defined(__MINGW32__) || defined(__MINGW64__)
-  char clang[25] = { 0 };
+  char clang[25] = {0};
   LANGID l;  /* ==> WORD */
   l = GetUserDefaultUILanguage();
   sprintf(clang, "%d", l);
   strncpy(puf, clang , 24);
-  hb_retc( puf );
-  free( puf );
+  hb_retc(puf);
+  free(puf);
 #else
   /* "Mecker" vom Compiler vermeiden */
-  strcpy( puf, "-1" );
-  hb_retc( puf );
-  free( puf );
+  strcpy(puf, "-1");
+  hb_retc(puf);
+  free(puf);
 #endif
 }
 
-  void hwg_strdebuglog(char * dest )
+void hwg_strdebuglog(char * dest)
 /* writes a string in logfile ac.log, only for debugging */
 {
-   size_t n;
    char c;
    char aus[1024] = {0};
-   for ( n = 0; n < sizeof ( (char *) dest) ; ++n ) {
-        c =  dest[n];
-        c ? sprintf(aus,"'%c' ", c) : sprintf(aus,"'\\0' ");
-        hwg_writelog( NULL ,  aus);
-    }
+   for( size_t n = 0; n < sizeof(static_cast<char*>(dest)) ; ++n )
+   {
+      c = dest[n];
+      c ? sprintf(aus, "'%c' ", c) : sprintf(aus, "'\\0' ");
+      hwg_writelog(nullptr, aus);
+   }
 }
-/* ====================== EOF of inter.c ======================= */
