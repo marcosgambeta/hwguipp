13437ad1824995b93f8c12ef2cb28a9f5d7f5c09
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2022-08-23 02:58:38 -0300

    Add tooltip.cpp

--- source/winapi/control.cpp
+++ source/winapi/control.cpp
@@ -28,9 +28,6 @@ WINUSERAPI HWND WINAPI GetAncestor(HWND hwnd, UINT gaFlags);
 HB_EXTERN_END
 #endif
 
-#ifndef TTS_BALLOON
-   #define TTS_BALLOON             0x40    // added by MAG
-#endif
 #ifndef CCM_SETVERSION
    #define CCM_SETVERSION (CCM_FIRST + 0x7)
 #endif
@@ -55,9 +52,7 @@ LRESULT APIENTRY TrackSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM l
 LRESULT APIENTRY TreeViewSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 static void CALLBACK s_timerProc(HWND, UINT, UINT, DWORD);
 
-static HWND s_hWndTT = nullptr;
 static bool s_lInitCmnCtrl = false;
-static bool s_lToolTipBalloon = false;
 static WNDPROC wpOrigTrackProc, wpOrigListProc, wpOrigTreeViewProc;
 
 /*
@@ -222,81 +217,6 @@ HB_FUNC( HWG_INITSTATUS )
 HB_FUNC( HWG_GETNOTIFYSBPARTS )
 {
    hb_retnl(static_cast<LONG>(((static_cast<NMMOUSE*>(HB_PARHANDLE(1)))->dwItemSpec)));
-}
-
-HB_FUNC( HWG_ADDTOOLTIP )
-{
-   HWND hWnd = hwg_par_HWND(1);
-   TOOLINFO ti;
-   int iStyle = 0;
-   void * hStr;
-
-   if( s_lToolTipBalloon )
-   {
-      iStyle = TTS_BALLOON;
-   }
-
-   if( !s_hWndTT )
-   {
-      s_hWndTT = CreateWindow(TOOLTIPS_CLASS, nullptr, WS_POPUP | TTS_ALWAYSTIP | iStyle,
-         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-         nullptr, nullptr, GetModuleHandle(nullptr), nullptr);
-   }
-   if( !s_hWndTT )
-   {
-      hb_retl(false);
-      return;
-   }
-   memset(&ti, 0, sizeof(TOOLINFO));
-   ti.cbSize = sizeof(TOOLINFO);
-   ti.uFlags = TTF_SUBCLASS | TTF_IDISHWND;
-   ti.hwnd = GetParent(static_cast<HWND>(hWnd));
-   ti.uId = reinterpret_cast<UINT_PTR>(hWnd);
-   ti.hinst = GetModuleHandle(nullptr);
-   ti.lpszText = const_cast<PTSTR>(HB_PARSTR(2, &hStr, nullptr));
-
-   hb_retl(SendMessage(s_hWndTT, TTM_ADDTOOL, 0, reinterpret_cast<LPARAM>(static_cast<LPTOOLINFO>(&ti))));
-   hb_strfree(hStr);
-}
-
-HB_FUNC( HWG_DELTOOLTIP )
-{
-   HWND hWnd = hwg_par_HWND(1);
-   TOOLINFO ti;
-
-   if( s_hWndTT )
-   {
-      memset(&ti, 0, sizeof(TOOLINFO));
-      ti.cbSize = sizeof(TOOLINFO);
-      ti.uFlags = TTF_IDISHWND;
-      ti.hwnd = GetParent(static_cast<HWND>(hWnd));
-      ti.uId = reinterpret_cast<UINT_PTR>(hWnd);
-      ti.hinst = GetModuleHandle(nullptr);
-
-      SendMessage(s_hWndTT, TTM_DELTOOL, 0, reinterpret_cast<LPARAM>(static_cast<LPTOOLINFO>(&ti)));
-   }
-}
-
-HB_FUNC( HWG_SETTOOLTIPTITLE )
-{
-   HWND hWnd = hwg_par_HWND(1);
-
-   if( s_hWndTT )
-   {
-      TOOLINFO ti;
-      void * hStr;
-
-      ti.cbSize = sizeof(TOOLINFO);
-      ti.uFlags = TTF_IDISHWND;
-      ti.hwnd = GetParent(static_cast<HWND>(hWnd));
-      ti.uId = reinterpret_cast<UINT_PTR>(hWnd);
-      ti.hinst = GetModuleHandle(nullptr);
-      //ti.lpszText = static_cast<LPTSTR>(HB_PARSTR(3, &hStr, nullptr));
-      ti.lpszText = const_cast<LPTSTR>(HB_PARSTR(2, &hStr, nullptr));
-
-      hb_retl(SendMessage(s_hWndTT, TTM_SETTOOLINFO, 0, reinterpret_cast<LPARAM>(static_cast<LPTOOLINFO>(&ti))));
-      hb_strfree(hStr);
-   }
 }
 
 HB_FUNC( HWG_GETTIMEPICKER )
@@ -723,22 +643,6 @@ HB_FUNC( HWG_SETCURSOR )
 HB_FUNC( HWG_GETCURSOR )
 {
    HB_RETHANDLE(GetCursor());
-}
-
-HB_FUNC( HWG_GETTOOLTIPHANDLE )
-{
-   HB_RETHANDLE(s_hWndTT);
-}
-
-HB_FUNC( HWG_SETTOOLTIPBALLOON )
-{
-   s_lToolTipBalloon = hb_parl(1);
-   s_hWndTT = nullptr;
-}
-
-HB_FUNC( HWG_GETTOOLTIPBALLOON )
-{
-   hb_retl(s_lToolTipBalloon);
 }
 
 HB_FUNC( HWG_REGPANEL )
---
+++ source/winapi/tooltip.cpp
@@ -0,0 +1,121 @@
+/*
+ * HWGUI - Harbour Win32 GUI library source code:
+ * C level controls functions
+ *
+ * Copyright 2001 Alexander S.Kresin <alex@kresin.ru>
+ * www - http://www.kresin.ru
+*/
+
+#define HB_OS_WIN_32_USED
+
+#define OEMRESOURCE
+#include "hwingui.h"
+#include <commctrl.h>
+#include <winuser.h>
+
+#include "hbapiitm.h"
+#include "hbvm.h"
+#include "hbdate.h"
+#include "hbtrace.h"
+
+/* Suppress compiler warnings */
+#include "incomp_pointer.h"
+#include "warnings.h"
+
+#ifndef TTS_BALLOON
+   #define TTS_BALLOON             0x40    // added by MAG
+#endif
+
+static HWND s_hWndTT = nullptr;
+static bool s_lToolTipBalloon = false;
+
+HB_FUNC( HWG_ADDTOOLTIP )
+{
+   HWND hWnd = hwg_par_HWND(1);
+   TOOLINFO ti;
+   int iStyle = 0;
+   void * hStr;
+
+   if( s_lToolTipBalloon )
+   {
+      iStyle = TTS_BALLOON;
+   }
+
+   if( !s_hWndTT )
+   {
+      s_hWndTT = CreateWindow(TOOLTIPS_CLASS, nullptr, WS_POPUP | TTS_ALWAYSTIP | iStyle,
+         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+         nullptr, nullptr, GetModuleHandle(nullptr), nullptr);
+   }
+   if( !s_hWndTT )
+   {
+      hb_retl(false);
+      return;
+   }
+   memset(&ti, 0, sizeof(TOOLINFO));
+   ti.cbSize = sizeof(TOOLINFO);
+   ti.uFlags = TTF_SUBCLASS | TTF_IDISHWND;
+   ti.hwnd = GetParent(static_cast<HWND>(hWnd));
+   ti.uId = reinterpret_cast<UINT_PTR>(hWnd);
+   ti.hinst = GetModuleHandle(nullptr);
+   ti.lpszText = const_cast<PTSTR>(HB_PARSTR(2, &hStr, nullptr));
+
+   hb_retl(SendMessage(s_hWndTT, TTM_ADDTOOL, 0, reinterpret_cast<LPARAM>(static_cast<LPTOOLINFO>(&ti))));
+   hb_strfree(hStr);
+}
+
+HB_FUNC( HWG_DELTOOLTIP )
+{
+   HWND hWnd = hwg_par_HWND(1);
+   TOOLINFO ti;
+
+   if( s_hWndTT )
+   {
+      memset(&ti, 0, sizeof(TOOLINFO));
+      ti.cbSize = sizeof(TOOLINFO);
+      ti.uFlags = TTF_IDISHWND;
+      ti.hwnd = GetParent(static_cast<HWND>(hWnd));
+      ti.uId = reinterpret_cast<UINT_PTR>(hWnd);
+      ti.hinst = GetModuleHandle(nullptr);
+
+      SendMessage(s_hWndTT, TTM_DELTOOL, 0, reinterpret_cast<LPARAM>(static_cast<LPTOOLINFO>(&ti)));
+   }
+}
+
+HB_FUNC( HWG_SETTOOLTIPTITLE )
+{
+   HWND hWnd = hwg_par_HWND(1);
+
+   if( s_hWndTT )
+   {
+      TOOLINFO ti;
+      void * hStr;
+
+      ti.cbSize = sizeof(TOOLINFO);
+      ti.uFlags = TTF_IDISHWND;
+      ti.hwnd = GetParent(static_cast<HWND>(hWnd));
+      ti.uId = reinterpret_cast<UINT_PTR>(hWnd);
+      ti.hinst = GetModuleHandle(nullptr);
+      //ti.lpszText = static_cast<LPTSTR>(HB_PARSTR(3, &hStr, nullptr));
+      ti.lpszText = const_cast<LPTSTR>(HB_PARSTR(2, &hStr, nullptr));
+
+      hb_retl(SendMessage(s_hWndTT, TTM_SETTOOLINFO, 0, reinterpret_cast<LPARAM>(static_cast<LPTOOLINFO>(&ti))));
+      hb_strfree(hStr);
+   }
+}
+
+HB_FUNC( HWG_GETTOOLTIPHANDLE )
+{
+   HB_RETHANDLE(s_hWndTT);
+}
+
+HB_FUNC( HWG_SETTOOLTIPBALLOON )
+{
+   s_lToolTipBalloon = hb_parl(1);
+   s_hWndTT = nullptr;
+}
+
+HB_FUNC( HWG_GETTOOLTIPBALLOON )
+{
+   hb_retl(s_lToolTipBalloon);
+}
