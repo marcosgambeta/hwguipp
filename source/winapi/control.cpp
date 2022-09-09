fdc3e1322c4571b8479fbeacac13e41ada6b6365
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2022-09-09 03:13:01 -0300

    Add panel.cpp (from control.cpp)

--- source/winapi/control.cpp
+++ source/winapi/control.cpp
@@ -84,22 +84,6 @@ HB_FUNC( HWG_MOVEWINDOW )
 }
 
 /*
-   CreatePanel(hParentWindow, nPanelControlID, nStyle, x1, y1, nWidth, nHeight)
-*/
-HB_FUNC( HWG_CREATEPANEL )
-{
-   HWND hWndPanel = CreateWindow(TEXT("PANEL"),   /* predefined class  */
-         nullptr,                  /* no window title   */
-         WS_CHILD | WS_VISIBLE | SS_GRAYRECT | SS_OWNERDRAW | CCS_TOP | hb_parnl(3),
-         hwg_par_int(4), hwg_par_int(5),  hwg_par_int(6), hwg_par_int(7),
-         hwg_par_HWND(1),
-         reinterpret_cast<HMENU>(static_cast<UINT_PTR>(hb_parni(2))),
-         GetModuleHandle(nullptr), nullptr);
-   HB_RETHANDLE(hWndPanel);
-   // SS_ETCHEDHORZ
-}
-
-/*
    CreateOwnBtn(hParentWIndow, nBtnControlID, x, y, nWidth, nHeight)
 */
 HB_FUNC( HWG_CREATEOWNBTN )
@@ -330,30 +314,6 @@ HB_FUNC( HWG_SETCURSOR )
 HB_FUNC( HWG_GETCURSOR )
 {
    HB_RETHANDLE(GetCursor());
-}
-
-HB_FUNC( HWG_REGPANEL )
-{
-   static bool bRegistered = false;
-
-   if( !bRegistered )
-   {
-      WNDCLASS wndclass;
-
-      wndclass.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW | CS_DBLCLKS;
-      wndclass.lpfnWndProc = DefWindowProc;
-      wndclass.cbClsExtra = 0;
-      wndclass.cbWndExtra = 0;
-      wndclass.hInstance = GetModuleHandle(nullptr);
-      wndclass.hIcon = nullptr;
-      wndclass.hCursor = LoadCursor(nullptr, IDC_ARROW);
-      wndclass.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_3DFACE + 1);
-      wndclass.lpszMenuName = nullptr;
-      wndclass.lpszClassName = TEXT("PANEL");
-
-      RegisterClass(&wndclass);
-      bRegistered = true;
-   }
 }
 
 HB_FUNC( HWG_REGOWNBTN )
---
+++ source/winapi/panel.cpp
@@ -0,0 +1,63 @@
+/*
+ * HWGUI - Harbour Win32 GUI library source code:
+ * C level controls functions
+ *
+ * Copyright 2001 Alexander S.Kresin <alex@kresin.ru>
+ * www - http://www.kresin.ru
+*/
+
+#define HB_OS_WIN_32_USED
+
+#define OEMRESOURCE
+#include "hwingui.h"
+#include <commctrl.h>
+#include <winuser.h>
+
+#include "hbapiitm.h"
+#include "hbvm.h"
+#include "hbdate.h"
+#include "hbtrace.h"
+
+/* Suppress compiler warnings */
+#include "incomp_pointer.h"
+#include "warnings.h"
+
+/*
+   CreatePanel(hParentWindow, nPanelControlID, nStyle, x1, y1, nWidth, nHeight)
+*/
+HB_FUNC( HWG_CREATEPANEL )
+{
+   HWND hWndPanel = CreateWindow(TEXT("PANEL"),   /* predefined class  */
+         nullptr,                  /* no window title   */
+         WS_CHILD | WS_VISIBLE | SS_GRAYRECT | SS_OWNERDRAW | CCS_TOP | hb_parnl(3),
+         hwg_par_int(4), hwg_par_int(5),  hwg_par_int(6), hwg_par_int(7),
+         hwg_par_HWND(1),
+         reinterpret_cast<HMENU>(static_cast<UINT_PTR>(hb_parni(2))),
+         GetModuleHandle(nullptr), nullptr);
+   HB_RETHANDLE(hWndPanel);
+   // SS_ETCHEDHORZ
+}
+
+HB_FUNC( HWG_REGPANEL )
+{
+   static bool bRegistered = false;
+
+   if( !bRegistered )
+   {
+      WNDCLASS wndclass;
+
+      wndclass.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW | CS_DBLCLKS;
+      wndclass.lpfnWndProc = DefWindowProc;
+      wndclass.cbClsExtra = 0;
+      wndclass.cbWndExtra = 0;
+      wndclass.hInstance = GetModuleHandle(nullptr);
+      wndclass.hIcon = nullptr;
+      wndclass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+      wndclass.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_3DFACE + 1);
+      wndclass.lpszMenuName = nullptr;
+      wndclass.lpszClassName = TEXT("PANEL");
+
+      RegisterClass(&wndclass);
+      bRegistered = true;
+   }
+}
